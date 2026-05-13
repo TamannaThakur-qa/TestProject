6eb45cff4f3789dff1727d9ae1b6e0f5acab2f06
Tamanna.Thakur <Tamanna.Thakur@defactoinfotech.com>
2026-05-13 11:49:25 +0530

    commit the chnages to gitub

---
+++ tests/testcode.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test');
+
+test('Login to OrangeHRM', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/');
+  await page.fill('input[name="username"]', 'Admin');
+  await page.fill('input[name="password"]', 'admin123');
+  await page.click('button[type="submit"]');
+  await expect(page).toHaveURL(/dashboard/);
+});
