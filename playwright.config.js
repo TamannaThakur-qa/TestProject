0e67b7f3b7e155bd194facaeb8b5c5df23dbb463
Tamanna.Thakur <Tamanna.Thakur@defactoinfotech.com>
2026-05-13 15:30:38 +0530

    TRY

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -23,7 +24,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+reporter: [
+  ['html'],
+  ['junit', { outputFile: 'test-results/results.xml' }]
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
