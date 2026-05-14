e16bb9efecf8b3592f21bd524715cfdc3f15f9b8
Tamanna.Thakur <Tamanna.Thakur@defactoinfotech.com>
2026-05-14 14:33:04 +0530

    test ss changes

--- playwright.config.js
+++ playwright.config.js
@@ -29,14 +29,21 @@ reporter: [
   ['html'],
   ['junit', { outputFile: 'test-results/results.xml' }]
 ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+  /* Base URL to use in actions like `await page.goto('')`. */
+  // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+  /* Capture screenshot on failure */
+  screenshot: 'only-on-failure',
+
+  /* Record video on failure */
+  video: 'retain-on-failure',
+
+  /* Collect trace when retrying the failed test */
+  trace: 'on-first-retry',
+},
 
   /* Configure projects for major browsers */
   projects: [
